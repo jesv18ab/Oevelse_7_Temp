ff4cf739395aacebebf3324d1897276c06d7d70f
my name <myemailid@domain.com>
2020-09-22 21:50:34 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,ScrollView, Text, View,TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>Dette er App.js filen</Text>
+        </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
